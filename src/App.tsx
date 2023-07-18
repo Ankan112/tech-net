294a2fd581b3a875d6ef190c0a3d5f4e48123238
Ankan112 <ankanhalder112@gmail.com>
2023-07-18 22:58:46 +0600

    project done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { onAuthStateChanged } from 'firebase/auth';
 import { Toaster } from './components/ui/Toaster';
 import MainLayout from './layouts/MainLayout';
+import { useAppDispatch } from './redux/hook';
+import { auth } from './lib/firebase';
+import { setLoading, setUser } from './redux/user/userSlice';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(setLoading(true));
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        dispatch(setUser(user.email!));
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  }, [dispatch]);
   return (
     <div>
       <Toaster />
